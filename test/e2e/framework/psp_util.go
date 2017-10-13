7e77418bd1fca2c69f6dacbc06f522d27b9748d6
Tim Allclair <tallclair@google.com>
2017-10-13 15:40:05 -0700

    Add E2E tests for PodSecurityPolicy

--- test/e2e/framework/psp_util.go
+++ test/e2e/framework/psp_util.go
@@ -18,14 +18,44 @@ package framework
 
 import (
 	"fmt"
+	"sync"
 
 	rbacv1beta1 "k8s.io/api/rbac/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/onsi/ginkgo"
 )
 
 const (
 	PodSecurityPolicyPrivileged     = "privileged"
 	PodSecurityPolicyPrivilegedRole = "podsecuritypolicy:privileged"
 )
+
+var (
+	isPSPEnabledOnce sync.Once
+	isPSPEnabled     bool
+)
+
+func IsPodSecurityPolicyEnabled(f *Framework) bool {
+	isPSPEnabledOnce.Do(func() {
+		psp, err := f.ClientSet.ExtensionsV1beta1().PodSecurityPolicies().
+			Get(PodSecurityPolicyPrivileged, metav1.GetOptions{})
+		if err != nil {
+			Logf("Error getting PodSecurityPolicy %s; assuming PodSecurityPolicy is disabled: %v",
+				PodSecurityPolicyPrivileged, err)
+			isPSPEnabled = false
+		} else if psp == nil {
+			Logf("PodSecurityPolicy %s was not found; assuming PodSecurityPolicy is disabled.",
+				PodSecurityPolicyPrivileged)
+			isPSPEnabled = false
+		} else {
+			Logf("Found PodSecurityPolicy %s; assuming PodSecurityPolicy is enabled.",
+				PodSecurityPolicyPrivileged)
+			isPSPEnabled = true
+		}
+	})
+	return isPSPEnabled
+}
 
 func CreateDefaultPSPBinding(f *Framework, namespace string) {
 	By(fmt.Sprintf("Binding the %s PodSecurityPolicy to the default service account in %s",
